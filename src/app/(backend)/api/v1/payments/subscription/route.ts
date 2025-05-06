b359d38aa22eec35d7abe346740226d338f50e3e
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-05-05 22:44:29 -0300

    refactor: the create checkout endpoint

--- src/app/(backend)/api/v1/payments/subscription/route.ts
+++ src/app/(backend)/api/v1/payments/subscription/route.ts
@@ -4,23 +4,23 @@ import { supabaseServerClient } from "@/libs/supabase/server";
 import SubscriptionService from "@/services/subscription";
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const userId = searchParams.get("userId");
-
-  if (!userId || typeof userId !== "string") {
-    throw new Error("Invalid or missing userId");
-  }
-
-  const subscriptionService = new SubscriptionService(supabaseServerClient);
-  const subscription = await subscriptionService.getSubscriptionByUserId(
-    userId
-  );
-
-  if (!subscription) {
-    return NextResponse.json({ status: 204 });
-  }
-
   try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId || typeof userId !== "string") {
+      throw new Error("Invalid or missing userId");
+    }
+
+    const subscriptionService = new SubscriptionService(supabaseServerClient);
+    const subscription = await subscriptionService.getSubscriptionByUserId(
+      userId
+    );
+
+    if (!subscription) {
+      return NextResponse.json({ status: 204 });
+    }
+    
     return NextResponse.json(subscription, { status: 200 });
   } catch (error) {
     console.error(error);
