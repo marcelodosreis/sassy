208934195cdab93f241bcc99b2bef7725e416088
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-08-13 23:25:20 -0300

    Hotfix/sign in with provider 01 (#37)
    * hotfix: update capitalize function do valid empty
    
    * Update Libraries (#31)
    
    * chore: update libraries 01
    
    * chore: update libraries 02
    
    * chore: update tailwind to v4
    
    * chore: update libraries
    
    * chore: update stripe api version
    
    * refactor: stop using auth in client (#32)
    
    * chore: update libraries 01
    
    * chore: update libraries 02
    
    * chore: update tailwind to v4
    
    * chore: update libraries
    
    * chore: update stripe api version
    
    * refactor: renaming authService instance
    
    * feat: make login in api
    
    * feat: login via api
    
    * refactor: movint i18n hook to context
    
    * refactor: moving modal hook to context
    
    * refactor: moving toast hook to context
    
    * refactor: create endpoint for signin, signup and confirm signup and stop using supabase service in client
    
    * refactor: create forogt password endpoint and refactor errors logic in new-password
    
    * refactor: create /me endpoint
    
    * refactor: add all auth domain in backend providers
    
    * fix: removing all notification feature and component (BREAKING CHANGE)
    
    * feat: create hook for auth external providers in client
    
    * chore: removing notification from webhook
    
    * fix: rollback OAuth Component
    
    * fix: add oauth login in server

--- src/components/v1/OAuth.tsx
+++ src/components/v1/OAuth.tsx
@@ -1,61 +1,58 @@
+import { OAuthBridge } from "@/bridges/oauth";
 import { PROVIDERS_IMAGE_URL } from "@/constants/providers-image-url";
-import { supabase } from "@/libs/supabase/client";
-import AuthService from "@/services/auth";
 
 export default function OAuth() {
-    const authService = new AuthService(supabase);
+  const oauthBridge = new OAuthBridge();
 
-    const PROVIDERS_MAP = [
-        {
-            provider: 'Google',
-            logo: PROVIDERS_IMAGE_URL.Google,
-            onClick: () => authService.signInWithProvider('google')
-        },
-        {
-            provider: 'Facebook',
-            logo: PROVIDERS_IMAGE_URL.Facebook,
-            onClick: () => authService.signInWithProvider('facebook')
-        },
-        {
-            provider: 'Twitter',
-            logo: PROVIDERS_IMAGE_URL.Twitter,
-            onClick: () => authService.signInWithProvider('twitter')
-        }
-    ]
-    return (
-        <>
-            <div className="text-center text-sm text-gray-600">or</div>
+  const PROVIDERS_MAP = [
+    {
+      provider: "Google",
+      logo: PROVIDERS_IMAGE_URL.Google,
+      onClick: () => oauthBridge.execute("google"),
+    },
+    {
+      provider: "Facebook",
+      logo: PROVIDERS_IMAGE_URL.Facebook,
+      onClick: () => oauthBridge.execute("facebook"),
+    },
+    {
+      provider: "Twitter",
+      logo: PROVIDERS_IMAGE_URL.Twitter,
+      onClick: () => oauthBridge.execute("twitter"),
+    },
+  ];
 
-            <div className="mt-4 flex justify-center space-x-4">
-                {PROVIDERS_MAP.map(({ provider, logo, onClick }) => (
-                    <ProviderButton
-                        key={provider}
-                        provider={provider}
-                        logo={logo}
-                        onClick={onClick}
-                    />
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="text-center text-sm text-gray-600">or</div>
+
+      <div className="mt-4 flex justify-center space-x-4">
+        {PROVIDERS_MAP.map(({ provider, logo, onClick }) => (
+          <ProviderButton
+            key={provider}
+            provider={provider}
+            logo={logo}
+            onClick={onClick}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 type ProviderButtonProps = {
-    provider: string;
-    logo: string;
-    onClick: () => void;
-}
+  provider: string;
+  logo: string;
+  onClick: () => void;
+};
+
 function ProviderButton({ provider, logo, onClick }: ProviderButtonProps) {
-    return (
-        <button
-            className="p-2 border border-gray-300 rounded-md bg-white hover:bg-gray-50 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            onClick={onClick}
-        >
-            <img
-                src={logo}
-                alt={provider}
-                className="w-5 h-5"
-            />
-        </button>
-    )
+  return (
+    <button
+      className="p-2 border border-gray-300 rounded-md bg-white hover:bg-gray-50 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      onClick={onClick}
+    >
+      <img src={logo} alt={provider} className="w-5 h-5" />
+    </button>
+  );
 }
